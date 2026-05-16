525e5eee999dbe16dc0e6223dead207ae4ad1a19
Mulema Makoge <emly42@icloud.com>
2026-05-16 17:31:46 +0100

    Fix Flutter version in CI workflow to 3.24.0

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -15,6 +15,9 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.google.gms.google-services") version "4.4.1" apply false
+    }
 }
 
 plugins {
