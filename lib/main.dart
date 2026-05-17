9ba80ef39bad82965ffb33924ecd082b203d456b
Mulema Makoge <emly42@icloud.com>
2026-05-17 04:31:49 +0100

    Add Firebase App Distribution and Crashlytics integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  // Pass all uncaught Flutter errors to Crashlytics
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
   runApp(const CampusMeshApp());
-}
+} 
